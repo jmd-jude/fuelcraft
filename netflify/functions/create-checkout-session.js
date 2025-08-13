7d4e3816643b9af46badc287e03cf98183a460e9
jmd-jude <jude.hoffner@gmail.com>
2025-08-12 20:52:00 -0400

    ecomm v0.1 fix some errors

--- netflify/functions/create-checkout-session.js
+++ netflify/functions/create-checkout-session.js
@@ -1,46 +1,10 @@
-// netlify/functions/create-checkout-session.js
-// Simple test version
-
 exports.handler = async (event, context) => {
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: 'Function is working!' })
     };
-
-    if (event.httpMethod === 'OPTIONS') {
-        return { statusCode: 200, headers, body: '' };
-    }
-
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            headers,
-            body: JSON.stringify({ error: 'Method not allowed' }),
-        };
-    }
-
-    try {
-        console.log('Function called - this is working!');
-        
-        return {
-            statusCode: 200,
-            headers,
-            body: JSON.stringify({ 
-                success: true,
-                message: 'Test function working',
-                sessionId: 'test_session_123',
-                orderID: 'FC-TEST-' + Date.now(),
-                airtableRecordId: 'test_record_123'
-            }),
-        };
-
-    } catch (error) {
-        console.error('Error:', error);
-        return {
-            statusCode: 500,
-            headers,
-            body: JSON.stringify({ error: error.message }),
-        };
-    }
 };
